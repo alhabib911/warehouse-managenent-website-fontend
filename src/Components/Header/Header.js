70d0ecd7ab346b5baaf93bb71e6eac1512187a8f
alhabib911 <abdullahalhabib100@gmail.com>
2022-05-08 02:57:34 +0600

    navbar done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import auth from '../farebase.init';
 import './Header.css'
 import Logo from '../../Logo/Logo.png'
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     const [user] = useAuthState(auth)
@@ -16,44 +17,46 @@ const Header = () => {
             <div className='header-top-text'>
                 <p>Zayn & Myza Warehouse Management System</p>
             </div>
-            <div className='navbar'>
-                <div className='logo-container'>
-                    <Link to='/home'><img src={Logo} alt="" /></Link>
-                </div>
-                <nav>
-                    <Link to='/'>Home</Link>
-                    <Link to='/blogs'>Blogs</Link>
-                    <Link to='/products'>Products</Link>
-                </nav>
-                <nav>
-                    {
-                        user && 
-                        <Link to='/manage'>Manage Inventory</Link>
-                    }
-                    {
-                        user &&
-                        <Link to='/addproduct'>Add Item</Link>
-                    }
-                    {
-                        user ?
-                            <Link to='/login' onClick={handleSignOut}>Log Out</Link>
-                            :
-                            <Link to='/login'>Login</Link>
-                            
-                    }
-                    {
-                        user ?
-                            <Link to='/login' onClick={handleSignOut}></Link>
-                            :
-                            <Link to='/signup'>Sign Up</Link>
-                            
-                    }
-
-                    
-                </nav>
+            <div>
+                <Navbar collapseOnSelect expand="lg"  >
+                    <Container>
+                        <Link className='logo' to='/home'><img src={Logo} alt="" /></Link>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Link to='/'>Home</Link>
+                                <Link to='/blogs'>Blogs</Link>
+                            </Nav>
+                            <Nav>
+                                {
+                                    user &&
+                                    <Link to='/manage'>Manage Inventory</Link>
+                                }
+                                {
+                                    user &&
+                                    <Link to='/addproduct'>Add Item</Link>
+                                }
+                                {
+                                    user ?
+                                        <Link to='/login' onClick={handleSignOut}>Log Out</Link>
+                                        :
+                                        <Link to='/login'>Login</Link>
+                                }
+                                {
+                                    user ?
+                                        <Link to='/login' onClick={handleSignOut}></Link>
+                                        :
+                                        <Link to='/signup'>Sign Up</Link>
+                                }
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
             </div>
         </div>
     );
 };
 
 export default Header;
+
+
--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
+import './Loading.css'
 
 const Loading = () => {
     return (
-        <div>
+        <div className='spiner'>
             <Spinner animation="border" variant="primary" />
         </div>
     );
